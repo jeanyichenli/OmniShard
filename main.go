11efa87e6266ed1b4523cc2e6dad9d598975949d
Jean Li <jean841115@gmail.com>
2026-03-05 15:08:41 +0800

    feat: initialize redis client in main entrypoint
    Call redis.InitRedisClient() from main before executing the Cobra
    command tree so that Redis connectivity is established when the CLI
    starts.
    
    This centralizes Redis initialization in main.go and prepares the
    application to use a shared redis client across commands.

--- main.go
+++ main.go
@@ -3,8 +3,15 @@ Copyright © 2025 Yi-Chen (Jean) Li jean841115@gmail.com
 */
 package main
 
-import "github.com/jeanyichenli/FileUploadSystem/cmd"
+import (
+	"github.com/jeanyichenli/FileUploadSystem/cmd"
+	"github.com/jeanyichenli/FileUploadSystem/redis"
+)
 
 func main() {
+	// init redis
+	redis.InitRedisClient()
+
+	// execute cmds
 	cmd.Execute()
 }
